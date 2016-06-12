f0e3a94a35f66d82b91718b8250748d441536b33
Tom Parker <palfrey@tevp.net>
2016-06-12 12:09:05 +0100

    Functioning sync!

--- docket.py
+++ docket.py
@@ -7,6 +7,7 @@ from todoist.managers.labels import LabelsManager
 from flask_sqlalchemy import SQLAlchemy
 from .models import build_models
 import urlparse
+from datetime import datetime, timedelta
 
 config = yaml.safe_load(file('config.yaml', 'r'))
 app = Flask(__name__)
@@ -65,6 +66,7 @@ def beeminder_oauth():
 	goals = requests.get("https://www.beeminder.com/api/v1/users/me/goals.json?filter=frontburner&access_token=%s" % auth_token)
 	existing.update_beeminder(goals.json(), auth_token)
 	db.session.commit()
+	flash("Logged into Beeminder")
 	return redirect(url_for('index'))
 
 @app.route("/beeminder/sync", methods=["POST"])
@@ -73,6 +75,7 @@ def beeminder_sync():
 	goals = requests.get("https://www.beeminder.com/api/v1/users/me/goals.json?filter=frontburner&access_token=%s" % existing.beeminder_access_token)
 	existing.update_beeminder(goals.json(), existing.beeminder_access_token)
 	db.session.commit()
+	flash("Beeminder synced")
 	return redirect(url_for('index'))
 
 @app.route("/todoist/sync", methods=['POST'])
@@ -81,6 +84,52 @@ def todoist_sync():
 	api = todoist.api.TodoistAPI(existing.todoist_access_token)
 	existing.update_todoist(api.sync(), existing.todoist_access_token)
 	db.session.commit()
+	flash("Todoist synced")
+	return redirect(url_for('index'))
+
+@app.route("/todoist/update", methods=['POST'])
+def todoist_update():
+	existing = User.query.filter_by(todoist_id=session["todoist_id"]).first()
+	api = todoist.api.TodoistAPI(existing.todoist_access_token)
+	existing.update_todoist(api.sync(), existing.todoist_access_token)
+	api._update_state(existing.todoist)
+	lm = todoist.managers.labels.LabelsManager(api)
+	if "beeminder" not in [x.data['name'] for x in lm.all()]:
+		lm.add("beeminder")
+		api.commit()
+	beeminder_label = [x for x in lm.all() if x.data['name'] == "beeminder"][0].data['id']
+	pm = todoist.managers.projects.ProjectsManager(api)
+	if "Beeminder" not in [x.data['name'] for x in pm.all()]:
+		pm.add("Beeminder")
+		api.commit()
+	else:
+		project = [x for x in existing.todoist['projects'] if x['name'] == "Beeminder"][0]
+		if project['is_deleted'] == 1:
+			raise Exception, project
+	beeminder_project = [x for x in pm.all() if x.data['name'] == "Beeminder"][0].data['id']
+	goals = existing.beeminder_goals
+	im = todoist.managers.items.ItemsManager(api)
+	existing_names = [
+		x['content'] for x in existing.todoist['items']
+		if beeminder_label in x['labels']]
+	for goal in goals:
+		title = goal['title']
+		losedate = goal['losedate']
+		when = datetime.fromtimestamp(losedate)
+		if when.hour < 5: # assume a early morning fail time, and so use previous day
+			when = when.date() - timedelta(days = 1)
+		else:
+			when = when.date()
+		if title in existing_names:
+			item = [x for x in existing.todoist['items'] if x['content'] == title][0]
+			if item['project_id'] != beeminder_project:
+				im.move({item['project_id']:[item['id']]}, beeminder_project)
+			im.update(item['id'], date_string=when.strftime("%Y/%m/%d"), checked=0, in_history=0, project_id = beeminder_project)
+		else:
+			item = im.add(title, beeminder_project, date_string=when.strftime("%Y/%m/%d"), labels = [beeminder_label])
+	api.commit()
+	db.session.commit()
+	flash("All goals updated in Todoist")
 	return redirect(url_for('index'))
 
 if __name__ == "__main__":
