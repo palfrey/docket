c4d3e92e7fa89c92cd5e08cf72022d05130cd584
Tom Parker <palfrey@tevp.net>
2016-06-12 14:01:51 +0100

    No point storing old data, only need when syncing

--- docket.py
+++ docket.py
@@ -49,12 +49,11 @@ def todoist_oauth():
 	todoist_id = data["user"]["id"]
 	existing = User.query.filter_by(todoist_id=todoist_id).all()
 	if len(existing) == 0:
-		user = User(todoist_id, data, access_token)
+		user = User(todoist_id, access_token)
+		user.update()
 		db.session.add(user)
 	elif len(existing) > 1:
 		raise Exception, ("Weird things", existing)
-	else:
-		existing[0].update_todoist(data, access_token)
 	db.session.commit()
 	session["todoist_id"] = todoist_id
 	return redirect(url_for('index'))
@@ -62,55 +61,37 @@ def todoist_oauth():
 @app.route("/beeminder/oauth", methods=["GET"])
 def beeminder_oauth():
 	existing = User.query.filter_by(todoist_id=session["todoist_id"]).first()
-	auth_token = request.args["access_token"]
-	goals = requests.get("https://www.beeminder.com/api/v1/users/me/goals.json?filter=frontburner&access_token=%s" % auth_token)
-	existing.update_beeminder(goals.json(), auth_token)
+	existing.update_beeminder(request.args["access_token"])
 	db.session.commit()
 	flash("Logged into Beeminder")
-	return redirect(url_for('index'))
-
-@app.route("/beeminder/sync", methods=["POST"])
-def beeminder_sync():
-	existing = User.query.filter_by(todoist_id=session["todoist_id"]).first()
-	goals = requests.get("https://www.beeminder.com/api/v1/users/me/goals.json?filter=frontburner&access_token=%s" % existing.beeminder_access_token)
-	existing.update_beeminder(goals.json(), existing.beeminder_access_token)
-	db.session.commit()
-	flash("Beeminder synced")
-	return redirect(url_for('index'))
-
-@app.route("/todoist/sync", methods=['POST'])
-def todoist_sync():
-	existing = User.query.filter_by(todoist_id=session["todoist_id"]).first()
-	api = todoist.api.TodoistAPI(existing.todoist_access_token)
-	existing.update_todoist(api.sync(), existing.todoist_access_token)
-	db.session.commit()
-	flash("Todoist synced")
 	return redirect(url_for('index'))
 
 @app.route("/todoist/update", methods=['POST'])
 def todoist_update():
 	existing = User.query.filter_by(todoist_id=session["todoist_id"]).first()
 	api = todoist.api.TodoistAPI(existing.todoist_access_token)
-	existing.update_todoist(api.sync(), existing.todoist_access_token)
-	api._update_state(existing.todoist)
+	data = api.sync()
 	lm = todoist.managers.labels.LabelsManager(api)
 	if "beeminder" not in [x.data['name'] for x in lm.all()]:
 		lm.add("beeminder")
 		api.commit()
 	beeminder_label = [x for x in lm.all() if x.data['name'] == "beeminder"][0].data['id']
+
 	pm = todoist.managers.projects.ProjectsManager(api)
 	if "Beeminder" not in [x.data['name'] for x in pm.all()]:
 		pm.add("Beeminder")
 		api.commit()
 	else:
-		project = [x for x in existing.todoist['projects'] if x['name'] == "Beeminder"][0]
+		project = [x for x in data['projects'] if x['name'] == "Beeminder"][0]
 		if project['is_deleted'] == 1:
 			raise Exception, project
 	beeminder_project = [x for x in pm.all() if x.data['name'] == "Beeminder"][0].data['id']
-	goals = existing.beeminder_goals
+
+	goals = requests.get("https://www.beeminder.com/api/v1/users/me/goals.json?filter=frontburner&access_token=%s" % existing.beeminder_access_token).json()
+
 	im = todoist.managers.items.ItemsManager(api)
 	existing_names = [
-		x['content'] for x in existing.todoist['items']
+		x['content'] for x in data['items']
 		if beeminder_label in x['labels']]
 	for goal in goals:
 		title = goal['title']
@@ -121,13 +102,14 @@ def todoist_update():
 		else:
 			when = when.date()
 		if title in existing_names:
-			item = [x for x in existing.todoist['items'] if x['content'] == title][0]
+			item = [x for x in data['items'] if x['content'] == title][0]
 			if item['project_id'] != beeminder_project:
 				im.move({item['project_id']:[item['id']]}, beeminder_project)
 			im.update(item['id'], date_string=when.strftime("%Y/%m/%d"), checked=0, in_history=0, project_id = beeminder_project)
 		else:
 			item = im.add(title, beeminder_project, date_string=when.strftime("%Y/%m/%d"), labels = [beeminder_label])
 	api.commit()
+	existing.update()
 	db.session.commit()
 	flash("All goals updated in Todoist")
 	return redirect(url_for('index'))
--- models.py
+++ models.py
@@ -4,35 +4,23 @@ import humanize
 def build_models(db):
     class User(db.Model):
         todoist_id = db.Column(db.Integer, primary_key=True)
-        todoist = db.Column(db.PickleType)
         todoist_access_token = db.Column(db.String(40), unique=True)
-        todoist_last_update = db.Column(db.DateTime)
-
+        last_update = db.Column(db.DateTime)
         beeminder_access_token = db.Column(db.String(25), unique=True)
-        beeminder_goals = db.Column(db.PickleType)
-        beeminder_last_update = db.Column(db.DateTime)
 
-        def __init__(self, todoist_id, todoist, todoist_access_token):
+        def __init__(self, todoist_id, todoist_access_token):
             self.todoist_id = todoist_id
-            self.todoist = todoist
             self.todoist_access_token = todoist_access_token
             self.todoist_last_update = datetime.datetime.now()
 
-        def todoist_pretty_update(self):
-            return humanize.naturaltime(datetime.datetime.now() - self.todoist_last_update)
+        def pretty_update(self):
+            return humanize.naturaltime(datetime.datetime.now() - self.last_update)
 
-        def update_todoist(self, todoist, todoist_access_token):
-            self.todoist = todoist
-            self.todoist_access_token = todoist_access_token
-            self.todoist_last_update = datetime.datetime.now()
+        def update(self):
+            self.last_update = datetime.datetime.now()
 
-        def beeminder_pretty_update(self):
-            return humanize.naturaltime(datetime.datetime.now() - self.beeminder_last_update)
-
-        def update_beeminder(self, beeminder_goals, beeminder_access_token):
-            self.beeminder_goals = beeminder_goals
+        def update_beeminder(self, beeminder_access_token):
             self.beeminder_access_token = beeminder_access_token
-            self.beeminder_last_update = datetime.datetime.now()
 
         def __repr__(self):
             return '<User %r>' % self.todoist_id
